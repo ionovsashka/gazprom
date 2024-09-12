9ce90547a998998ef955a85df15e81e2bc80aa0c
Alexander Ionov <aionov.unium@gmail.com>
2024-09-12 16:12:16 +0500

    dsghsdsdh

--- src/app/modules/pages/training/pages/cyber-security/container/cyber-security.component.ts
+++ src/app/modules/pages/training/pages/cyber-security/container/cyber-security.component.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from "@angular/common";
-import { ChangeDetectionStrategy } from "@angular/core";
+import { ChangeDetectionStrategy, inject } from "@angular/core";
 import { Component } from '@angular/core';
 import { CyberSecurityInstructionComponent } from "../components/cyber-security-instruction/cyber-security-instruction.component";
 import { Insctuction } from "../../cyber-security/models/insctuction.model";
 import { CyberSecurityRoutingModule } from "../cyber-security-routing.module";
+import { Router } from "@angular/router";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,21 +14,44 @@ import { CyberSecurityRoutingModule } from "../cyber-security-routing.module";
   styleUrl: 'cyber-security.component.scss'
 })
 export class CyberSecurityComponent {
+  //region DI
+
+  private readonly _router: Router = inject(Router);
+
+  //endregion
   //region Fields
 
-  /**
-   * Список инструкций.
-   */
-  readonly instructions: Insctuction[] = [
-    {
-      fileLink: "file:////home/aionov/untitled/src/assets/instructions/cyber-security-instruction_1.pdf",
-      name: "Инструкция по кибербезопасности_1"
-    },
-    {
-      fileLink: "file:////home/aionov/untitled/src/assets/instructions/cyber-security-instruction_2.pdf",
-      name: "Инструкция по кибербезопасности_2"
-    },
-  ]
+  currentQuestion: number = 1;
+
+  errorCode: number | null = null;
+
+  showSuccessMessage: boolean = false;
+
+  //endregion
+  //region Public
+
+  answerOnQuestion(status: boolean) {
+    if(!status) {
+      this.errorCode = this.randomNumber(1,4);
+      return;
+    }
+
+    this.showSuccessMessage = true;
+  }
+
+randomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+nextBtnClickHandler() {
+  if (this.currentQuestion === 3) {
+    this._router.navigate(["training"])
+    return;
+  }
+
+  this.currentQuestion++;
+  this.showSuccessMessage = false;
+}
 
   //endregion
 }
